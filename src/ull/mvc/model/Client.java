5bb738d637593be298f08f4e6b08a5278ac1ce8c
Orlandy Ariel <alu0100773408@ull.edu.es>
2016-12-06 17:04:32 +0000

    diagram update

--- src/ull/mvc/model/Client.java
+++ src/ull/mvc/model/Client.java
@@ -5,58 +5,49 @@ package ull.mvc.model;
  *
  */
 public class Client
-{
+{	//Attributes
 	private String firtsName;
 	private String lastName;
 	private String address;
 	private long telephone;
 	private Prduct favoriteProduct;
-	
+	//getters and setters
 	public String getFirtsName()
 	{
 		return firtsName;
 	}
-
 	public void setFirtsName(String firtsName)
 	{
 		this.firtsName = firtsName;
 	}
-
 	public String getLastName()
 	{
 		return lastName;
 	}
-
 	public void setLastName(String lastName)
 	{
 		this.lastName = lastName;
 	}
-
 	public String getAddress()
 	{
 		return address;
 	}
-
 	public void setAddress(String address)
 	{
 		this.address = address;
 	}
-
 	public long getTelephone()
 	{
 		return telephone;
 	}
-
 	public void setTelephone(long telephone)
 	{
 		this.telephone = telephone;
 	}
-
 	public Prduct getFavoriteProduct()
 	{
 		return favoriteProduct;
 	}
-
 	public void setFavoriteProduct(Prduct favoriteProduct)
 	{
 		this.favoriteProduct = favoriteProduct;
@@ -66,5 +57,4 @@ public class Client
 	{
 		return "First Name:"+firtsName+", Last name:"+lastName+", Address:"+address+", Telephone:"+telephone+", Favorite Product:"+favoriteProduct;
 	}
-	
 }
